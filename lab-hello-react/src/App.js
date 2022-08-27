6a866b3770a9b7c43f3c9ee62dae8a9b863a9aff
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-08-27 14:29:19 +0200

    encore plus done

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -13,33 +13,33 @@ function App() {
     <div className="App">
       <header className= "App-header section">
         <nav>
-          <img id="logo" className="navImg" src={logo}  alt=""></img>
-          <img id="logo2" className="navImg" src={logo2} alt=""></img>
+          <img id="logo" className="navImg" src={logo}  alt="" />
+          <img id="logo2" className="navImg" src={logo2} alt="" />
         </nav>
         <div className="content">
           <h1>Say Hello to ReactJs</h1>
           <p>You'll learn how to use the most popular frontend library, and become a super ninhja dev.</p>
-          <a className="button">Awesome !</a>
+          <a href="https://fr.reactjs.org/" className="button">Awesome !</a>
         </div>
       </header>
       <footer className="section">
         <div className="product">
-          <img className="image" src={image1} alt=""></img>
+          <img className="image" src={image1} alt="" />
           <h2>Declarative</h2>
           <p>React makes it <br />painless to create <br />interactive UIs</p>
         </div>
         <div className="product">
-          <img className="image" src={image2} alt=""></img>
+          <img className="image" src={image2} alt="" />
           <h2>Components</h2>
           <p>Build encapsuled <br />components that <br />manage their state</p>
         </div>
         <div className="product">
-          <img className="image" src={image3} alt=""></img>
+          <img className="image" src={image3} alt="" />
           <h2>Single Way</h2>
           <p>A set of immutable <br />values are passed <br />to the component's</p>
         </div>
         <div className="product">
-          <img className="image" src={image4} alt=""></img>
+          <img className="image" src={image4} alt="" />
           <h2>JSX</h2>
           <p>Statically-typed <br />designed to run <br />on modern browsers</p>
         </div>
